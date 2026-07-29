557e58cdb6ccc017bc5c9a65118ac5903aef0937
Dysthesis <antheoraviel@protonmail.com>
2026-07-29 19:13:36 +1000

    build(nix): add flake and formatter

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "KOReader development and package flake";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./nix/formatter.nix
+      ];
+
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    };
+}
